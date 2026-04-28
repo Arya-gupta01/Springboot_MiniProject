4e7553c84082aabdb5d5b17cdb10b3801ed4df85
Arya-gupta01 <guptaarya0101@gmail.com>
2026-04-28 15:03:50 +0530

    add some changes in mapper file

--- Employee-Management-Sys-main/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Employee.java
+++ Employee-Management-Sys-main/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/model/Employee.java
@@ -1,6 +1,11 @@
 package com.example.EmployeeManagementSystem.model;
+//
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column ;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 
 
